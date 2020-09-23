56da83b193a5687e180582f65359e62cca3f5e95
ZgraTimHSLU <tim.zgraggen@stud.hslu.ch>
2020-09-23 10:23:22 +0200

    test for if-else

---
+++ 30_Exercises/SW02/datatypeSizes.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main( int argc, char* argv[] ) {
+
+	printf("Size of Char is : %ld\n", sizeof(char));
+	printf("Size of short is : %ld\n", sizeof(short));
+	printf("Size of int is : %ld\n", sizeof(int));
+	return
+
+}
+
+
